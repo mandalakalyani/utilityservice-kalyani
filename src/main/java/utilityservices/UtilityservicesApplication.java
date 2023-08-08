d9df1713461f750f02dda8fce3181384eb9d4fd9
kalyani <kalyanimandala2604@gmail.com>
2023-08-08 21:55:13 +0530

    added the jar file

---
+++ src/main/java/utilityservices/UtilityservicesApplication.java
@@ -0,0 +1,13 @@
+package utilityservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utilityservices/UtilityservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package utilityservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
